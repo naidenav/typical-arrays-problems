3c4a9546a253d9c2a7aa06d12fde31627fb80460
Alexander Naiden <naidenav@gmail.com>
2021-02-22 00:08:20 +0300

    feat: implement 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    let result;
+    if (!array || array.length === 0) {
+        result = 0;
+    } else {
+        let sortArray = array.sort(function(a,b) {
+            return a - b;
+        });
+        result = sortArray[0];
+    }
+    return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let result;
+    if (!array || array.length === 0) {
+        result = 0;
+    } else {
+        let sortArray = array.sort(function(a,b) {
+            return a - b;
+        });
+        result = sortArray[array.length -1];
+    }
+    return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let result;
+    if (!array || array.length === 0) {
+        result = 0;
+    } else {
+    let sumArray = array.reduce(function(sum, current) {
+        return sum + current;
+    }, 0);
+    result = sumArray / array.length;
+    }
+
+    return result;
 }
